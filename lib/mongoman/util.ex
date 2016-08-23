094cc9e768de0e8035e192d2ab1f3e6dbc50f88d
Christian Howe <chowe@thenewoffice.com>
2016-08-23 18:45:01 -0500

    Add choose_port util function

--- lib/mongoman/util.ex
+++ lib/mongoman/util.ex
@@ -1,5 +1,17 @@
 defmodule Mongoman.Util do
-  def port_available?(port) do
+  def choose_port do
+    try_port 27017
+  end
+
+  defp try_port(port) do
+    if port_available?(port) do
+      port
+    else
+      try_port(port + 1)
+    end
+  end
+
+  defp port_available?(port) do
     case :gen_tcp.listen(port, []) do
       {:ok, port} ->
         :ok = :gen_tcp.close(port)
