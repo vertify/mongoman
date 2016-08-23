3b4123f6201c5f1f40c7ecdd16acdc857237c97a
Christian Howe <chowe@thenewoffice.com>
2016-08-23 18:37:48 -0500

    Add port_available? util function

--- lib/mongoman/types.ex
+++ lib/mongoman/types.ex
@@ -1,6 +1,6 @@
 defmodule Mongoman.Instance do
   @type t :: %__MODULE__{hostname: String.t, port: 0..65535}
-  defstruct [:hostname, :port]
+  defstruct [:path, :hostname, :port]
 end
 
 defmodule Mongoman.ReplicaSet do
---
+++ lib/mongoman/util.ex
@@ -0,0 +1,13 @@
+defmodule Mongoman.Util do
+  def port_available?(port) do
+    case :gen_tcp.listen(port, []) do
+      {:ok, port} ->
+        :ok = :gen_tcp.close(port)
+        true
+      {:error, :eaddrinuse} ->
+        false
+      _ ->
+        false # it's safer to assume a port is in use upon failure
+    end
+  end
+end
